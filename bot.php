291a11f778599ac7ac4187b06f056b9ff1383451
surya-id <44961458+surya-id@users.noreply.github.com>
2020-07-07 17:35:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@
  * THE SOFTWARE.
  *
  * @package	CodeIgniter
- * @author	EllisLab Dev Team
+ * @author	EllisLab Surya Team
  * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
  * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
  * @license	https://opensource.org/licenses/MIT	MIT License
